2983f299c7c17cdbf72ef1202742f81a927ed6d7
seoyoungseung <areema@naver.com>
2021-04-05 17:27:42 +0900

    fix

--- app/src/main/java/com/jiran/mylibrarytest/MainTest.kt
+++ app/src/main/java/com/jiran/mylibrarytest/MainTest.kt
@@ -11,8 +11,8 @@ class MainTest {
                 .baseUrl("https://api.github.com/")
                 .build()
 
-        val service: GitHubService = retrofit.create(GitHubService::class.java)
-        val repose = service.listRepos("octocat")
-        Log.d("MainTest", "printTest: ${repose}")
+//        val service: GitHubService = retrofit.create(GitHubService::class.java)
+//        val repose = service.listRepos("octocat")
+//        Log.d("MainTest", "printTest: ${repose}")
     }
 }
